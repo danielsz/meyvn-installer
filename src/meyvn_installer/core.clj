6792c7e14d653e16a31c7e87918ad83e0a755c09
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2024-07-24 18:44:01 +0300

    Bump version

--- src/meyvn_installer/core.clj
+++ src/meyvn_installer/core.clj
@@ -7,7 +7,7 @@
   (:import [java.nio.file Paths LinkOption]
            [java.io FileNotFoundException]))
 
-(def version "1.8.0")
+(def version "1.8.1")
 (def release (str (System/getProperty "user.home") "/.m2/repository/org/meyvn/meyvn/" version "/meyvn-" version ".jar"))
 
 
